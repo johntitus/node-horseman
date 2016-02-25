johntitus/node-horseman

commit 2d0afef16658446d62792d3bb31643ca2fdc3de2

Make `.waitForSelector` use jQuery (if available)
Fixes #86

lib/actions.js
@@ -1054,10 +1054,10 @@ exports.waitForNextPage = function() {
 
 exports.waitForSelector = function(selector) {
 	debug('.waitForSelector()', selector);
-	eval("var elementPresent = function() {" +
-		"  var element = document.querySelector('" + selector + "');" +
-		"  return (element ? true : false);" +
-		"};");
+	var elementPresent = function elementPresent() {
+		var els = window.jQuery ? jQuery(selector) : document.querySelectorAll(selector);
+		return els.length > 0;
+	};
 	return this.waitFor(elementPresent, true).then(function() {
 		debug('.waitForSelector() complete');
 	});

commit 35299050599c4df2a46553aae29d2b3a46d0e5dc

Fix `.waitForSelector`
Broken by 2d0afef16658446d62792d3bb31643ca2fdc3de2, but the tests were passing.
Making timouts reject "fixed" the tests.

lib/actions.js
@@ -1069,10 +1069,11 @@ exports.waitForNextPage = function() {
 
 exports.waitForSelector = function(selector) {
 	debug('.waitForSelector()', selector);
-	var elementPresent = function elementPresent() {
-		var els = window.jQuery ? jQuery(selector) : document.querySelectorAll(selector);
-		return els.length > 0;
-	};
+	var elementPresent = new Function(
+		'var selector = "' + selector + '";' +
+		'var els = window.jQuery ? jQuery(selector) : document.querySelectorAll(selector);' +
+		'return els.length > 0;'
+	);
 	return this.waitFor(elementPresent, true).then(function() {
 		debug('.waitForSelector() complete');
 	});