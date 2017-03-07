77f245afef6b4de23791cdb36b11be087f048c81
Alex Layton <alex@layton.in>
2017-03-07 14:37:40 -0500

    Fix links.js example
    closes #253

--- examples/links.js
+++ examples/links.js
@@ -25,11 +25,11 @@ function hasNextPage(){
 }
 
 function scrape(){
-	
+
 	return new Promise( function( resolve, reject ){
 		return getLinks()
 		.then(function(newLinks){
-			
+
 			links = links.concat(newLinks);
 
 			if ( links.length < 30 ){
@@ -40,7 +40,7 @@ function scrape(){
 							.click("#pnnext")
 							.wait(1000)
 							.then( scrape );
-					} 
+					}
 				});
 			}
 		})
@@ -52,7 +52,7 @@ horseman
 	.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
 	.open("http://www.google.com")
 	.type("input[name='q']","horseman")
-	.click("button:contains('Google Search')")
+	.click("input[value='Google Search']")
 	.keyboardEvent("keypress",16777221)
 	.waitForSelector("div.g")
 	.then( scrape )
